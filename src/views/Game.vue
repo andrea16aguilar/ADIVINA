de965635ba41b6e8f9aae3fb2a22b3f7370500f4
andrea16aguilar <andrea16aguilarasah@gmail.com>
2019-10-27 17:46:07 -0600

    actualizacion

---
+++ src/views/Game.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <h1> HOLA API</h1>
+        <button @click="llamarAPI()">MOSTRAAAAA</button>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            arrayTrivia:[],
+        }
+    },
+    methods:{
+        llamarAPI(){
+            this.arrayTrivia.push();
+            fetch('https://opentdb.com/api.php?amount&type=boolean')
+  .then(function(response) {
+      console.log(response)
+    return response.json();
+  })
+  .then(function(questions) {
+questions.results.forEach(function(question) {
+    this.arrayTrivia.push( {
+        pregunta: question.question,
+        respuesta: question.answer, 
+        tipo: question.type,
+    })
+  console.log(element);
+});
+    console.log(question)
+  });
+        }
+    },
+}
+
+</script>
---
+++ src/views/Religion.vue
@@ -0,0 +1,71 @@
+<template>
+    <div>
+        <h1>
+            CATEGORIA RELIGION.
+        </h1>
+        <H2>
+NIVEL FACIL
+        </H2>
+        <div>
+  <div>
+    <b-card-group deck>
+      <b-card bg-variant="primary" text-variant="white" header="Primary" class="text-center">
+        <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+      </b-card>
+
+      <b-card bg-variant="secondary" text-variant="white" header="Secondary" class="text-center">
+        <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+      </b-card>
+
+      <b-card bg-variant="success" text-variant="white" header="Success" class="text-center">
+        <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+      </b-card>
+    </b-card-group>
+  </div>
+  <div class="mt-3">
+    <b-card-group deck>
+      <b-card bg-variant="info" text-variant="white" header="Info" class="text-center">
+        <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+      </b-card>
+
+      <b-card bg-variant="warning" text-variant="white" header="Warning" class="text-center">
+        <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+      </b-card>
+
+      <b-card bg-variant="danger" text-variant="white" header="Danger" class="text-center">
+        <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+      </b-card>
+    </b-card-group>
+  </div>
+  <div class="mt-3">
+    <b-card-group deck>
+      <b-card bg-variant="light" header="Light" class="text-center">
+        <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+      </b-card>
+
+      <b-card bg-variant="dark" header="Dark" text-variant="white" class="text-center">
+        <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+      </b-card>
+
+      <b-card header="Default" class="text-center">
+        <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+      </b-card>
+        <b-card bg-variant="primary" text-variant="white" header="Primary" class="text-center">
+        <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+      </b-card>
+    </b-card-group>
+  </div>
+</div>
+<b-button variant="info" @click=" llamarreligiondificil()">SIGUIENTE NIVEL</b-button>
+    </div>
+</template>
+
+<script>
+export default {
+  methods:{
+    llamarreligiondificil(){
+      this.$router.push('./religiondificil')
+    }
+  }
+}
+</script>
